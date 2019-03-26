2b03e1548ad27bdf953c2aa3591014bc3fc6d2c5
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-25 23:13:03 -0400

    Add first tests and also commit old deletions

--- app.py
+++
@@ -1,5 +0,0 @@
-import os
-from create_app import create_app
-
-app = create_app(os.environ['APP_SETTINGS'])
-
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,2 +1,9 @@
 import pytest
+from app.create_app import create_app
+from app.config import TestingConfig
+
+@pytest.fixture
+def app():
+    app = create_app(TestingConfig)
+    return app
 
