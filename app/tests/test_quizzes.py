e373e8a481f12d5b15f51a29fd1940fd5c9dbf77
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-26 00:41:04 -0400

    Improved basic quiz test

--- app/tests/test_quizzes.py
+++ app/tests/test_quizzes.py
@@ -1,10 +1,13 @@
+import pytest
 from app.models import Quiz
 
-def test_create_quiz(db):
-    quiz = Quiz('sample', False)
+@pytest.mark.parametrize('title', ['sample', 'asdfghfds,ghjg'])
+@pytest.mark.parametrize('enabled', [True, False])
+def test_create_quiz(db, title, enabled):
+    quiz = Quiz(title, enabled)
     db.session.add(quiz)
     db.session.commit()
 
     quiz = Quiz.query.get(quiz.id)
-    assert quiz.title == 'sample'
-    assert quiz.enabled == False
+    assert quiz.title == title
+    assert quiz.enabled == enabled
