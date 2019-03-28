0815a8baa54b446b796f7be575fa777723ffdf9a
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-27 23:10:32 -0400

    Refactor out general create API code

--- app/create_app.py
+++ app/create_app.py
@@ -12,9 +12,19 @@ class Routes:
     CREATE_CHOICE = "/admin/create/choice"
 
 # Gets fields out of request for create and editing 
-def request_vals(request, *keys):
+def request_vals(*keys):
     data = request.get_json()
     return {k: data[k] for k in keys}
+
+def create_api(model_cls, *keys):
+    model = model_cls()
+    for attr, val in request_vals(*keys).items():
+        setattr(model, attr, val)
+
+    db.session.add(model)
+    db.session.commit()
+    return str(model.id)
+
 
 def create_app(config):
     app = Flask(__name__)
@@ -29,34 +39,16 @@ def create_app(config):
     @app.route(Routes.CREATE_QUIZ, methods=['POST'])
     def create_quiz():
         # TODO admin auth
-        quiz = Quiz()
-        for attr, val in request_vals(request, 'title').items():
-            setattr(quiz, attr, val)
-
-        db.session.add(quiz)
-        db.session.commit()
-        return str(quiz.id)
+        return create_api(Quiz, 'title')
 
     @app.route(Routes.CREATE_QUESTION, methods=['POST'])
     def create_question():
         # TODO admin auth
-        question = Question()
-        for attr, val in request_vals(request, 'text', 'position', 'quiz_id').items():
-            setattr(question, attr, val)
-
-        db.session.add(question)
-        db.session.commit()
-        return str(question.id)
+        return create_api(Question, 'text', 'position', 'quiz_id')
 
     @app.route(Routes.CREATE_CHOICE, methods=['POST'])
     def create_choice():
         # TODO admin auth
-        choice = Choice() 
-        for attr, val in request_vals(request, 'text', 'correct', 'question_id').items():
-            setattr(choice, attr, val)
-
-        db.session.add(choice)
-        db.session.commit()
-        return str(choice.id)
+        return create_api(Choice, 'text', 'correct', 'question_id')
 
     return app
